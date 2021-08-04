a128bc02ad4eb35b27cf83aeec3b9a09574887ef
yosse95ai <karateyosi@gmail.com>
2021-08-04 11:08:05 +0900

    Add: 日ごとの天気予報関数追加 (#6)

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { observable, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
@@ -10,14 +10,19 @@ export class WeatherService {
 
   private urlBase: string = environment.weatherUrlBase + "appid=" + environment.OPEN_WEATHERT_MAP;
   private hourlyUrl: string = environment.hourlyOneCallUrl + "appid=" + environment.OPEN_WEATHERT_MAP;
-  constructor(private http: HttpClient) {   }
+  private dailyUrl: string = environment.dailyOneCallUrl + "appid=" + environment.OPEN_WEATHERT_MAP;
+  constructor(private http: HttpClient) { }
 
   getWeather(lat: number, lon: number): Observable<any> {
     let url: string = this.urlBase + "&lat=" + lat + "&lon=" + lon;
     return this.http.get(url);
   }
-  getHourlyWeather(lat: number, lon: number): Observable<any>{
+  getHourlyWeather(lat: number, lon: number): Observable<any> {
     let url: string = this.hourlyUrl + "&lat=" + lat + "&lon=" + lon;
+    return this.http.get(url);
+  }
+  getDailyWeather(lat: number, lon: number): Observable<any> {
+    let url: string = this.dailyUrl + "&lat=" + lat + "&lon=" + lon;
     return this.http.get(url);
   }
 }
